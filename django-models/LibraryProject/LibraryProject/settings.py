48e3a56054e7b0a912757027960c75e446c93c6f
ahmed elbahy <exbahy@gmail.com>
2025-09-05 23:13:54 +0300

    Final check and push for Task 0: Advanced Model Relationships

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf', # <<<<<<<<<<<<<<<<< أضف هذا السطر هنا <<<<<<<<<<<<<<<<<
-    'relationship_app', # <<<<<<<<<<<<<<<<< أضف هذا السطر هنا <<<<<<<<<<<<<<<<<
-
 ]
 
 MIDDLEWARE = [
