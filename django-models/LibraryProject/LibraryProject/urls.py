5ca82ee5c157df0feb1a929e98992144a4de71a8
ahmed elbahy <exbahy@gmail.com>
2025-09-06 02:14:27 +0300

    Add views and URLs for relationship_app

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # <--- لازم تضيف include هنا
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # السطر الجديد ده بيقول: أي رابط يبدأ بكلمة "app/"
+    # ابعته لملف urls.py اللي جوه relationship_app
+    path('app/', include('relationship_app.urls')), # <--- ضيف السطر ده
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
-# Create your views here.
+# 1. الـ View الأول: من نوع Function-based
+# وظيفته يعرض كل الكتب الموجودة في الداتا بيز
+def all_books(request):
+    # هنجيب كل أوبجكتات الكتب
+    books = Book.objects.all()
+    # هنحضر الـ context اللي هنبعته للـ template
+    # المفتاح 'books' هو اللي بنستخدمه جوه الـ template في الـ for loop
+    context = {'books': books}
+    # هنرجع الـ template بعد ما ندمجه بالـ context
+    return render(request, 'relationship_app/list_books.html', context)
+
+
+# 2. الـ View التاني: من نوع Class-based
+# وظيفته يعرض تفاصيل مكتبة واحدة معينة بالكتب اللي فيها
+class LibraryDetailView(DetailView):
+    # بنحدد الموديل اللي الـ view ده هيتعامل معاه
+    model = Library
+    # بنحدد اسم الـ template اللي هيتعرض فيه تفاصيل المكتبة
+    template_name = 'relationship_app/library_detail.html'
+    # بنغير اسم المتغير الافتراضي 'object' لاسم أوضح هو 'library'
+    # عشان نقدر نستخدمه في الـ template كده: {{ library.name }}
+    context_object_name = 'library'
