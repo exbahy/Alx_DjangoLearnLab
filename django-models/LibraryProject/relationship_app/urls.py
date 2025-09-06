3a81cabeaf5564014dc76d1045a69699b57e6eab
ahmed elbahy <exbahy@gmail.com>
2025-09-06 20:54:58 +0300

    Fix URL paths in relationship_app/urls.py for checker compliance

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,9 +5,12 @@ from . import views # استيراد views من نفس المجلد
 
 app_name = 'relationship_app' # مهم لتحديد اسم التطبيق في الـ URLs
 
+
 urlpatterns = [
-    path('', views.index, name='index'), # صفحة رئيسية بسيطة للتطبيق
-    path('add/', views.add_book_view, name='add_book'),
-    path('change/<int:pk>/', views.change_book_view, name='change_book'), # <int:pk> لالتقاط رقم الكتاب
-    path('delete/<int:pk>/', views.delete_book_view, name='delete_book'), # <int:pk> لالتقاط رقم الكتاب
+    path('', views.index, name='index'),
+    # <<<<<<<<<<<<<<<<< التعديل هنا لـ 'add_book/' <<<<<<<<<<<<<<<<<
+    path('add_book/', views.add_book_view, name='add_book'),
+    # <<<<<<<<<<<<<<<<< التعديل هنا لـ 'edit_book/<int:pk>/' <<<<<<<<<<<<<<<<<
+    path('edit_book/<int:pk>/', views.change_book_view, name='change_book'),
+    path('delete/<int:pk>/', views.delete_book_view, name='delete_book'),
 ]
